19bac0677542fd92eaee88056dedbbdb1eb6ff08
Julian Fenk <julian@nastymatter.net>
2015-08-03 09:05:07 +0200

    Added defines for database metadata.

--- db.c
+++ db.c
@@ -1,5 +1,13 @@
 #include "db.h"
 
+//TODO: find better name
+#define TABLE_NAME "Bargeld"
+
+#define COLUMN_ID "id"
+#define COLUMN_DATE "date"
+#define COLUMN_PURPOSE "purpose"
+#define COLUMN_EXPENSE "expense"
+
 sqlite3 *openDatabase(const char *path)
 {
   sqlite3 *db;
@@ -8,4 +16,17 @@ sqlite3 *openDatabase(const char *path)
     return 0;
 
   return db;
+}
+
+float dbGetSum()
+{
+  float sum = 0.001;
+  int result = 0;
+  sqlite3_stmt *statement;
+  sqlite3 *db = openDatabase(DATABASE_FILE);
+  
+  if(!db)
+    return sum;
+
+  result = sqlite3_prepare_v2(db, "SELECT SQLITE_VERSION()", -1, $
 }
